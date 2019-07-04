44dcbfc2f1336d07f3ee7443249bf0ca90f2ffb3
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2019-07-04 16:04:30 +0400

    Hooks finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Pet from "./Pet";
+
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cocktiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
       <SearchParams />
     </div>
   );
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animals", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error);
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      {/* <h1>{location}</h1> */}
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
@@ -16,6 +32,8 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
